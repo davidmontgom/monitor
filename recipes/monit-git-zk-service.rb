d6698398b93ce0cfdcd268426053c0200bd3ba6d
David Montgomery <davidmontgomery@gmail.com>
2015-10-18 16:08:28 +0700

    f

--- recipes/monit-git-zk-service.rb
+++ recipes/monit-git-zk-service.rb
@@ -3,7 +3,7 @@
 service "monit"
 template "/etc/monit/conf.d/git-service-monitor.conf" do
   path "/etc/monit/conf.d/git-service-monitor.conf"
-  source "monit.git-service-monitor.conf.erb"
+  source "monit.git-zk-service-monitor.conf.erb"
   owner "root"
   group "root"
   mode "0755"
