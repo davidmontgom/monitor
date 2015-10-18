306b37046b6e111555e4706cf3aa5732981c8f84
David Montgomery <davidmontgomery@gmail.com>
2015-10-18 15:52:25 +0700

    f

---
+++ recipes/git-zk-service.rb
@@ -0,0 +1,11 @@
+
+service "supervisord"
+template "/etc/supervisor/conf.d/git-service.conf" do
+  path "/etc/supervisor/conf.d/git-service.conf"
+  source "supervisord.git-zk-service.conf.erb"
+  owner "root"
+  group "root"
+  mode "0755"
+  notifies :restart, resources(:service => "supervisord")
+end
+ 
