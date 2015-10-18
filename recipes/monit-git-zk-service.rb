3beeb923acd631d78ea5756179b547de7ed49505
David Montgomery <davidmontgomery@gmail.com>
2015-10-18 15:49:53 +0700

    f

---
+++ recipes/monit-git-zk-service.rb
@@ -0,0 +1,11 @@
+
+
+service "monit"
+template "/etc/monit/conf.d/git-service-monitor.conf" do
+  path "/etc/monit/conf.d/git-service-monitor.conf"
+  source "monit.git-service-monitor.conf.erb"
+  owner "root"
+  group "root"
+  mode "0755"
+  notifies :restart, resources(:service => "monit")
+end
