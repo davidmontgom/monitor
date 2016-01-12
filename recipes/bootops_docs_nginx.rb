c07ee353228280d8001720fed16e12501429ee80
David Montgomery <davidmontgomery@gmail.com>
2016-01-13 06:32:58 +0800

    f

---
+++ recipes/bootops_docs_nginx.rb
@@ -0,0 +1,14 @@
+template "/etc/nginx/sites-available/docs.nginx.conf" do
+  path "/etc/nginx/sites-available/docs.nginx.conf"
+  source "bootops.nginx.conf.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  notifies :reload, resources(:service => "nginx")
+  variables :nginx_port => nginx_port, :uwsgi_port => uwsgi_port
+end
+
+link "/etc/nginx/sites-enabled/docs.nginx.conf" do
+  to "/etc/nginx/sites-available/docs.nginx.conf"
+end
+service "nginx"
